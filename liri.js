1313f7642054c49d28042c5fb6107f771c897765
Tasima <re.anderson617@gmail.com>
2018-10-08 21:46:27 -0400

    movie-this complete

--- liri.js
+++ liri.js
@@ -33,89 +33,135 @@ function runLiri() {
     // Print the name of venue, venue location, and date of event formatted as MM/DD/YYYY for all events returned
     // Expected searchTerm is the name of an artist
 
-    if (searchTerm) {
-      request(`https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`, function (error, response, body) {
-        
-      if (error){
+    if (!searchTerm) {
+      console.log("No search term found");
+      return false;
+    }
+
+    request(`https://rest.bandsintown.com/artists/${searchTerm}/events?app_id=codingbootcamp`, function (error, response, body) {
+
+      if (error) {
         console.log(`error: 
-        ${error}`); 
+${error}`);
         return false;
       }
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-        
-        const results = JSON.parse(body);
 
-        if (!results[0]) {
-          //If there's nothing to be found in the array, then state as such
-          console.log(`No results found for ${searchTerm}`)
-          return false;
-        }
+      const results = JSON.parse(body);
 
-        console.log(`Results for ${searchTerm}:
+      if (!results[0]) {
+        //If there's nothing to be found in the array, then state as such
+        console.log(`No results found for ${searchTerm}`)
+        return false;
+      }
+
+      console.log(`Results for ${searchTerm}:
         `)
-        // the results come back as an array of objects, so loop through that array and print what we want
-        results.forEach(function(result) {
-          console.log(`Venue: ${result.venue.name}
+      // the results come back as an array of objects, so loop through that array and print what we want
+      results.forEach(function (result) {
+        console.log(`Venue: ${result.venue.name}
 Location: ${result.venue.city}, ${result.venue.region}, ${result.venue.country}
 Date: ${moment(result.datetime, "YYYY-MM-DDTHH:mm:ss").format("MM/DD/YYYY")}
 --------------------------------------------------`)
-        })
       })
-    } else {
-      console.log("No artist entered")
-    }
+    })
   } else if (command === "spotify-this-song") {
     // Search for a song name on the Spotify API
     // Print the artist, song title, preview link from Spotif, and album
     // If no song is provided, default to "The Sign" by Ace of Base
-    if(!searchTerm) {
+    if (!searchTerm) {
       searchTerm = "The Sign Ace of Base"
     }
 
     spotify
-  .search({ type: 'track', query: searchTerm })
-  .then(function(response) {
-    const responseItems = response.tracks.items;
+      .search({
+        type: 'track',
+        query: searchTerm
+      })
+      .then(function (response) {
+        const responseItems = response.tracks.items;
 
-    if (!responseItems[0]) {
-      // If there aren't any items found,
-      console.log(`No results for ${searchTerm}`)
-      return false;
-    }
+        if (!responseItems[0]) {
+          // If there aren't any items found,
+          console.log(`No results for ${searchTerm}`)
+          return false;
+        }
 
-    
-    console.log(`Results for ${searchTerm}:
+
+        console.log(`Results for ${searchTerm}:
     `)
 
-    responseItems.forEach(function(song) {
-      let artist = song.artists[0].name;
-      
-      if (song.artists.length > 0) {
-        for (let i = 1; i < song.artists.length; i++) {
-          artist = `${artist}, ${song.artists[i].name}`
-        }
-      }
+        responseItems.forEach(function (song) {
+          let artist = song.artists[0].name;
 
-      const songTitle = song.name;
-      const prevLink = song.preview_url;
-      const songAlbum = song.album.name;
+          if (song.artists.length > 0) {
+            for (let i = 1; i < song.artists.length; i++) {
+              artist = `${artist}, ${song.artists[i].name}`
+            }
+          }
 
-      console.log(`Title: ${songTitle}
+          const songTitle = song.name;
+          const prevLink = song.preview_url;
+          const songAlbum = song.album.name;
+
+          console.log(`Title: ${songTitle}
 Artist: ${artist}
 Album: ${songAlbum}
 Preview: ${prevLink}
 --------------------------------------------------`)
-    })
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+        })
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
 
   } else if (command === "movie-this") {
     // Search OMDB api
     //Print movie title, year it came out, IMDB rating, Rotton Tomatoes rating, country where the movie was produced, the language, plot, and actors of the movie
     // If no movie is provided, default to the movie "Mr. Nobody"
+
+    if (!searchTerm) {
+      searchTerm = "Mr. Nobody"
+    }
+
+    request(`http://www.omdbapi.com/?apikey=trilogy&t=${searchTerm}`, function (error, response, body) {
+      if (error) {
+        console.log(`Error:
+  ${error}`)
+        return false;
+      }
+      const parsedBody = JSON.parse(body)
+
+      if (parsedBody.Response === "False") {
+        console.log(`No Results for ${searchTerm}`);
+        return false;
+      }
+
+      const title = parsedBody.Title;
+      const year = parsedBody.Year;
+      const country = parsedBody.Country;
+      const language = parsedBody.Language;
+      const plot = parsedBody.Plot;
+      const actors = parsedBody.Actors;
+
+      let ratings = ""
+      parsedBody.Ratings.forEach(function (rating) {
+        ratings += `${rating.Source} Rating: ${rating.Value}
+`
+      })
+      
+      console.log(`Result for ${searchTerm}:
+      
+Title: ${title}
+Year of Release: ${year}
+Actors: ${actors}
+Country of Production: ${country}
+Language: ${language}
+${ratings}Plot: ${plot}`)
+
+    })
+
   } else {
     // If none of the pre-programmed commands are inputted, print instructions
     console.log(`
